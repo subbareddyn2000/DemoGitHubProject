b46cc83b206f203727e604d0501cca8cc0717c8e
subba <subba@DESKTOP-KANN52V>
2022-12-08 10:15:48 +0530

    New script

--- src/dec2/Testcase1.java
+++ src/dec2/Testcase1.java
@@ -1,7 +1,22 @@
 package dec2;
 
 public class Testcase1 {
+public static float add(int a, int b)
+{
+	return b;
+	
+}
+public static float add(int a, float b)
+{
+	return b;
+	
+}
 
+public static float add(float a, int b)
+{
+	return b;
+	
+}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
